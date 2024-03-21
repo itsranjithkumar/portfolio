10d6e69bf57b4cd132085e38af1b40769c838136
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-03-21 17:15:31 +0530

    bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,10 +14,15 @@ showMenu('nav_toggle','nav_menu')
 
 // ACTIVE & REMOVE ACTIVE
 const navLink = document.querySelectorAll('.nav_link')
+navLink.forEach(n=> n.classList.remove('active'))
 
 function linkAction(){
-    navLink.forEach
-  }
+    navLink.forEach(n=> n.classList.remove('active'))
+    this.classList.add('active')
 
+    const navMenu = document.getElementById('nav_menu')
+    navMenu.classList.remove('show')
+  }
+ 
 
 navLink.forEach(n => n. addEventListener('click',linkAction))
